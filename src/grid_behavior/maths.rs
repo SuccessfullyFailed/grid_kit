fb0db23773dbcd229b238f9fc685f1348fb57b3e
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-04-09 15:27:29 +0200

    Rename 'addition' to 'modification' in grid maths

--- src/grid_behavior/maths.rs
+++ src/grid_behavior/maths.rs
@@ -3,13 +3,13 @@ macro_rules! impl_maths {
 		impl<T> std::ops::$trait<crate::Grid<T>> for crate::Grid<T> where T:std::ops::$trait<Output=T> + Clone + 'static {
 			type Output = crate::Grid<T>;
 		
-			fn $fn_name(mut self, addition:crate::Grid<T>) -> Self::Output {
+			fn $fn_name(mut self, modification:crate::Grid<T>) -> Self::Output {
 				let modification_function:&'static dyn Fn(T, T) -> T = $modification;
-				for y in 0..self.height.min(addition.height) {
-					for x in 0..self.width.min(addition.width) {
+				for y in 0..self.height.min(modification.height) {
+					for x in 0..self.width.min(modification.width) {
 						let left_index:usize = y * self.width + x;
-						let right_index:usize = y * addition.width + x;
-						self[left_index] = modification_function(self[left_index].clone(), addition[right_index].clone())
+						let right_index:usize = y * modification.width + x;
+						self[left_index] = modification_function(self[left_index].clone(), modification[right_index].clone())
 					}
 				}
 				self
@@ -20,13 +20,13 @@ macro_rules! impl_maths {
 macro_rules! impl_maths_assign {
 	($trait:ident, $fn_name:ident, $modification:expr) => {
 		impl<T> std::ops::$trait<crate::Grid<T>> for crate::Grid<T> where T:std::ops::$trait + Clone + 'static {
-			fn $fn_name(&mut self, addition:crate::Grid<T>) {
+			fn $fn_name(&mut self, modification:crate::Grid<T>) {
 				let modification_function:&'static dyn Fn(&mut T, T) = $modification;
-				for y in 0..self.height.min(addition.height) {
-					for x in 0..self.width.min(addition.width) {
+				for y in 0..self.height.min(modification.height) {
+					for x in 0..self.width.min(modification.width) {
 						let left_index:usize = y * self.width + x;
-						let right_index:usize = y * addition.width + x;
-						modification_function(&mut self[left_index], addition[right_index].clone())
+						let right_index:usize = y * modification.width + x;
+						modification_function(&mut self[left_index], modification[right_index].clone())
 					}
 				}
 			}
