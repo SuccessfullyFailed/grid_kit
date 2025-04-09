7ec99c75545156ab5bfb646cf06c69bc5bd83ee5
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-04-09 15:26:46 +0200

    Add basic assign grid maths

--- src/grid_behavior/maths.rs
+++ src/grid_behavior/maths.rs
@@ -17,7 +17,31 @@ macro_rules! impl_maths {
 		}
 	};
 }
+macro_rules! impl_maths_assign {
+	($trait:ident, $fn_name:ident, $modification:expr) => {
+		impl<T> std::ops::$trait<crate::Grid<T>> for crate::Grid<T> where T:std::ops::$trait + Clone + 'static {
+			fn $fn_name(&mut self, addition:crate::Grid<T>) {
+				let modification_function:&'static dyn Fn(&mut T, T) = $modification;
+				for y in 0..self.height.min(addition.height) {
+					for x in 0..self.width.min(addition.width) {
+						let left_index:usize = y * self.width + x;
+						let right_index:usize = y * addition.width + x;
+						modification_function(&mut self[left_index], addition[right_index].clone())
+					}
+				}
+			}
+		}
+	};
+}
+
+
+
 impl_maths!(Add, add, &|left, right| left.clone() + right.clone());
 impl_maths!(Sub, sub, &|left, right| left.clone() - right.clone());
 impl_maths!(Mul, mul, &|left, right| left.clone() * right.clone());
 impl_maths!(Div, div, &|left, right| left.clone() / right.clone());
+
+impl_maths_assign!(AddAssign, add_assign, &|left, right| *left += right);
+impl_maths_assign!(SubAssign, sub_assign, &|left, right| *left -= right);
+impl_maths_assign!(MulAssign, mul_assign, &|left, right| *left *= right);
+impl_maths_assign!(DivAssign, div_assign, &|left, right| *left /= right);
