6376a0f294f4619ae9ea0758b659d8f5434691c5
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-04-12 02:00:23 +0200

    Rename convert function to map and add a map_ref function

--- src/grid_behavior/converter.rs
+++ src/grid_behavior/converter.rs
@@ -5,11 +5,22 @@ use crate::Grid;
 impl<T> Grid<T> {
 
 	/// Convert the grid to another type.
-	pub fn convert<U, V>(self, conversion_function:V) -> Grid<U> where V:Fn(T) -> U + 'static {
+	pub fn map<U, V>(self, conversion_function:V) -> Grid<U> where V:Fn(T) -> U + 'static {
 		let width:usize = self.width;
 		let height:usize = self.height;
 		Grid {
 			data: self.into_iter().map(conversion_function).collect(),
+			width,
+			height
+		}
+	}
+
+	/// Convert the grid to another type without consuming self.
+	pub fn map_ref<U, V>(&self, conversion_function:V) -> Grid<U> where V:Fn(&T) -> U + 'static {
+		let width:usize = self.width;
+		let height:usize = self.height;
+		Grid {
+			data: self.iter().map(conversion_function).collect(),
 			width,
 			height
 		}
