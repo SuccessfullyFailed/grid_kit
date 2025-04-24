ccdb9e4ef793562b9df560644ce5f972c60ecd88
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-04-24 03:39:56 +0200

    Add grid file storage

---
+++ src/storage/file_conversion.rs
@@ -0,0 +1,23 @@
+use crate::{ Grid, ByteConversion };
+use file_ref::FileRef;
+use std::error::Error;
+
+
+
+impl<T> Grid<T> where T:ByteConversion {
+
+	/// Save the grid to a file.
+	pub fn save_to_file(&self, file_path:&str) -> Result<(), Box<dyn Error>> {
+		FileRef::new(file_path).write_bytes(&self.to_bytes())
+	}
+
+	/// Create a grid by reading a file.
+	pub fn read_from_file(file_path:&str) -> Result<Grid<T>, Box<dyn Error>> {
+		let file:FileRef = FileRef::new(file_path);
+		if !file.exists() {
+			return Err(format!("Could not read grid from file '{file_path}', file does not exist.").into());
+		}
+		let file_bytes:Vec<u8> = file.read_bytes()?;
+		Grid::<T>::from_bytes(&file_bytes)
+	}
+}
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -2,5 +2,7 @@ mod byte_conversion_t;
 mod byte_conversion_t_u;
 mod byte_conversion_grid;
 mod byte_conversion_grid_u;
+mod file_conversion;
+mod file_conversion_u;
 
 pub(crate) use byte_conversion_t::ByteConversion;
