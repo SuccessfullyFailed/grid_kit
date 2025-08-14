86a1edc688f8eaac514b104a2f212acfa25c437c
SuccessfullyFailed <renzogerritzen@gmail.com>
2025-08-14 02:29:17 +0200

    Add weighed path finding method

--- src/grid_parsing/pathing.rs
+++ src/grid_parsing/pathing.rs
@@ -1,6 +1,6 @@
+use std::{ error::Error, ops::Add };
 use crate::{ Grid, GridIndexer };
 use super::GridRegion;
-use std::error::Error;
 
 
 
@@ -25,8 +25,6 @@ impl GridRegion {
 		let bounds_grid:Grid<&bool> = self.bounds_sub_grid();
 		let start_index:usize = bounds_grid.xy_to_index(start_coord[0] - self.bounds[0], start_coord[1] - self.bounds[1]);
 		let end_index:usize = bounds_grid.xy_to_index(end_coord[0] - self.bounds[0], end_coord[1] - self.bounds[1]);
-
-
 		let max_x:usize = bounds_grid.width - 1;
 		let max_y:usize = bounds_grid.len() - bounds_grid.width;
 
@@ -79,5 +77,75 @@ impl<T> Grid<T> where T:PartialEq {
 	// Find a path from one index to another. Will only move over pixels that are equal to the starting pixel.
 	pub fn find_path<U, V>(&self, start:U, end:V) -> Result<Vec<[usize; 2]>, Box<dyn Error>> where U:GridIndexer, V:GridIndexer {
 		self.region_at_eq(start.to_grid_index(self)).find_path(start, end)
+	}
+}
+impl<T> Grid<T> {
+
+	// Find a path from one index to another. Will only move over pixels that are equal to the starting pixel.
+	pub fn find_path_weighed<U, V, W, X>(&self, start:U, end:V, weight_function:W) -> Result<Vec<[usize; 2]>, Box<dyn Error>> where U:GridIndexer, V:GridIndexer, W:Fn((usize, &T), (usize, &T)) -> Option<X>, X:Ord + Add<Output=X> + Clone + Default {
+
+		// Find and validate start and end.
+		let start_index:usize = start.to_grid_index(self);
+		let end_index:usize = end.to_grid_index(self);
+		if start_index >= self.len() {
+			return Err("Start coordinate falls outside of the grid.".into());
+		}
+		if end_index >= self.len() {
+			return Err("Start coordinate falls outside of the grid.".into());
+		}
+		let max_x:usize = self.width - 1;
+		let max_y:usize = self.len() - self.width;	
+
+		// Keep checking positions in the queue.
+		let mut origin_grid:Grid<Option<(usize, X)>> = Grid::new(vec![None; self.width * self.height], self.width, self.height); // For each node, keep the origin and value of total weight to get here.
+		origin_grid[start_index] = Some((start_index, X::default()));
+		let mut queue:Vec<(usize, X)> = Vec::with_capacity(self.width * self.height); // Has a lot of space, likely too much. Stops it from moving around in memory when growing.
+		queue.push((start_index, X::default()));
+		let mut queue_cursor:usize = 0; // Keep a cursor to prevent moving the entire queue through memory on resizing.
+		while queue_cursor < queue.len() {
+			let (current_index, current_weight) = &queue[queue_cursor];
+			let current_index:usize = *current_index;
+
+			// If end found, backtrack and return path.
+			if current_index == end_index {
+				let mut path_indexes:Vec<usize> = vec![current_index];
+				let mut backtrack_cursor:usize = current_index;
+				while let Some((previous_index, _)) = &origin_grid[backtrack_cursor] {
+					if path_indexes.contains(previous_index) {
+						return Err("Repeating loop in backtracking path.".into());
+					}
+					path_indexes.push(*previous_index);
+					if previous_index == &start_index {
+						break;
+					}
+					backtrack_cursor = *previous_index;
+				}
+				return Ok(path_indexes.into_iter().rev().map(|index| self.index_to_xy(index)).collect());
+			}
+
+			// Add neighbors to queue.
+			let current_value:&T = &self[current_index];
+			let mut queue_additions:Vec<(usize, X)> = Vec::with_capacity(4);
+			for neighbor_index in self._index_neighbors(current_index, max_x, max_y) {
+				if self.index_is_valid(neighbor_index) {
+					let neighbor_value:&T = &self[neighbor_index];
+					if let Some(weight_addition_to_neighbor) = weight_function((current_index, current_value), (neighbor_index, neighbor_value)) {
+						let value_to_neighbor:X = current_weight.clone() + weight_addition_to_neighbor;
+						let current_neighbor_origin:&Option<(usize, X)> = &origin_grid[neighbor_index];
+						if current_neighbor_origin.is_none() || value_to_neighbor < current_neighbor_origin.as_ref().unwrap().1 {
+							origin_grid[neighbor_index] = Some((current_index, value_to_neighbor.clone()));
+							queue_additions.push((neighbor_index, value_to_neighbor));
+						}
+					}
+				}
+			}
+			queue.extend(queue_additions);
+
+			// Move cursor to next item.
+			queue_cursor += 1;
+		}
+
+		// No path was found.
+		Err("Could not find path.".into())
 	}
 }
--- src/grid_parsing/pathing_u.rs
+++ src/grid_parsing/pathing_u.rs
@@ -21,8 +21,31 @@ mod test {
 		for index in &path {
 			compare_grid[*index] = 'x';
 		}
-		println!("[compare grid]\n{compare_grid}\n");
+		println!("[selected road grid]\n{compare_grid}\n");
 		
 		assert_eq!(path, vec![[1, 0], [1, 1], [0, 1], [0, 2], [0, 3], [0, 4], [1, 4], [2, 4], [2, 3], [3, 3], [4, 3], [4, 4], [5, 4], [6, 4], [7, 4], [7, 3], [8, 3], [9, 3], [9, 2], [9, 1], [9, 0]]);
+	}
+
+
+	#[test]
+	fn test_weighed_path_finding() {
+		let maze:[&str; 5] = [
+			" x   xxx x",
+			"xxxx     x",
+			"x o  xx  x",
+			"x xxx  xxx",
+			"xxx+xxxx  "
+		];
+
+		let grid:Grid<char> = Grid::new(maze.iter().map(|line| line.chars().collect::<Vec<char>>()).flatten().collect(), 10, 5);
+		println!("[grid]\n{grid}\n");
+		let path:Vec<[usize; 2]> = grid.find_path_weighed([1, 0], [9, 0], |(_left_index, _left), (_right_index, right)| match right { 'o' => Some(10), '+' => Some(2), 'x' => Some(1), _ => None }).unwrap();
+		let mut compare_grid:Grid<char> = Grid::new(vec![' '; grid.width * grid.height], grid.width, grid.height);
+		for index in &path {
+			compare_grid[*index] = 'x';
+		}
+		println!("[selected road grid]\n{compare_grid}\n");
+		
+		assert_eq!(path, vec![[1, 0], [1, 1], [0, 1], [0, 2], [0, 3], [0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [7, 3], [8, 3], [9, 3], [9, 2], [9, 1], [9, 0]]);
 	}
 }
