dbcc43ee57e1293b788ccaed29f815f09dee0425
SuccessfullyFailed <renzogerritzen@gmail.com>
2026-05-28 03:12:24 +0200

    Stop requiring FnMut for grid mapper function

--- src/grid_behavior/converter.rs
+++ src/grid_behavior/converter.rs
@@ -5,7 +5,7 @@ use crate::Grid;
 impl<T> Grid<T> {
 
 	/// Convert the grid to another type.
-	pub fn map<U, V>(self, mut conversion_function:V) -> Grid<U> where V:FnMut(T) -> U {
+	pub fn map<U, V>(self, conversion_function:V) -> Grid<U> where V:Fn(T) -> U {
 		let width:usize = self.width;
 		let height:usize = self.height;
 		Grid {
@@ -16,7 +16,7 @@ impl<T> Grid<T> {
 	}
 
 	/// Convert the grid to another type without consuming self.
-	pub fn map_ref<U, V>(&self, mut conversion_function:V) -> Grid<U> where V:FnMut(&T) -> U + {
+	pub fn map_ref<U, V>(&self, conversion_function:V) -> Grid<U> where V:Fn(&T) -> U + {
 		let width:usize = self.width;
 		let height:usize = self.height;
 		Grid {
